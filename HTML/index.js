97334405ffd336092bceda0b3ec9fac23b1cbae1
Vikas Chaudhary DICE <vikas@dice-academy.com>
2023-12-26 16:23:07 +0530

    code updated till css class 2

---
+++ HTML/index.js
@@ -0,0 +1,35 @@
+console.log("hello world !");
+/**
+ * pbius
+ * <pre>-preserve tag
+ * <big>
+ * <small>
+ * subscript,superscript
+ * charcter entity refernce-&;-reserved keyword 
+ * browser tools
+ * 1-view page source
+ * 2-inspect element
+ * 3-responsive design -diff screen size
+ * 4-live edit code
+ * 5-validating web page=validotr.w3.org
+ * 
+ * 
+ * semantic and non semantic tags
+ * -semantic tags-div tag is for section
+ * -header,footer,article,section,nav
+ * 
+ * body tags-header,main,footer,nav,aside
+ * aside-not that imporatnt
+ * 
+ * more tags-navigation tag
+ * <a href="#home">home</a>
+ * 
+ * table
+ * bodytags
+ * nav
+ * table 2
+ * checkbox
+ * 
+ * <text are>
+ * 
+ */
